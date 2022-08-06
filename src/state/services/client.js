ddd5a44306f16cf69ab8eb4e272cbb7807906b8e
THOMAS HARDING <hardingt604@gmail.com>
2022-08-06 15:45:09 -0500

    more github CIery

--- src/state/services/client.js
+++ src/state/services/client.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export default createClient(
-  https://tfsniozmyuuijscfdqre.supabase.co,
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmc25pb3pteXV1aWpzY2ZkcXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5OTMzNTYsImV4cCI6MTk2MzU2OTM1Nn0.o5Vjx7n791rkZxuhmxHUoti3dkFusShLHfaXhGvGNHg
+  'https://tfsniozmyuuijscfdqre.supabase.co',
+  // eslint-disable-next-line max-len
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmc25pb3pteXV1aWpzY2ZkcXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5OTMzNTYsImV4cCI6MTk2MzU2OTM1Nn0.o5Vjx7n791rkZxuhmxHUoti3dkFusShLHfaXhGvGNHg'
 );
