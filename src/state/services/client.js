6e03aa04d1a8b6aa4990dd0169ec48c50153fa40
THOMAS HARDING <hardingt604@gmail.com>
2022-08-06 15:43:12 -0500

    maybe github ci likes this more?

--- src/state/services/client.js
+++ src/state/services/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export default createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  https://tfsniozmyuuijscfdqre.supabase.co,
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmc25pb3pteXV1aWpzY2ZkcXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5OTMzNTYsImV4cCI6MTk2MzU2OTM1Nn0.o5Vjx7n791rkZxuhmxHUoti3dkFusShLHfaXhGvGNHg
 );
